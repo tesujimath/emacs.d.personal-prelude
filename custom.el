568e2e0a0b16ffe08e5c929f4a738503bc7e3517
Simon Guest <simon.guest@agresearch.co.nz>
2025-01-14 08:37:06 +1300

    Customize xterm mouse mode on

--- custom.el
+++ custom.el
@@ -28,7 +28,8 @@
      (sequence "WAITING(w@/!)" "IN-PROGRESS(i!)" "|" "CANCELLED(c@/!)")))
  '(package-selected-packages
    '(color-theme-sanityinc-tomorrow envrc deadgrep clojure-mode yaml-mode web-mode lsp-ui lsp-mode json-mode js2-mode rainbow-mode elisp-slime-nav rainbow-delimiters company consult orderless vertico exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number nlinum move-text magit projectile imenu-anywhere hl-todo guru-mode git-modes git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major crux browse-kill-ring anzu ag ace-window emacsql-sqlite))
- '(whitespace-line-column 1000))
+ '(whitespace-line-column 1000)
+ '(xterm-mouse-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
