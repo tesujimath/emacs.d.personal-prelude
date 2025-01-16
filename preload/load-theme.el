1c183dcb25514a39a2283d27ed735543cc141428
Simon Guest <simon.guest@agresearch.co.nz>
2025-01-17 08:55:49 +1300

    Fix custom theme installation

--- preload/load-theme.el
+++ preload/load-theme.el
@@ -4,6 +4,10 @@
 
 ;;; Code:
 
+;; To install and load a custom theme we need this dependency here
+;; see https://github.com/bbatsov/prelude/pull/802
+(require 'prelude-packages)
+(prelude-require-package 'color-theme-sanityinc-tomorrow)
 (setq prelude-theme 'sanityinc-tomorrow-night)
 
 (provide 'load-theme)
