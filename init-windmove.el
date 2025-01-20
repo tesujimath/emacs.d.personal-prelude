97f3b82022f2e1147afcca449ff6aa08a03a6e47
Simon Guest <simon.guest@agresearch.co.nz>
2025-01-21 12:25:58 +1300

    Globally disable windmove

---
+++ init-windmove.el
@@ -0,0 +1,11 @@
+;;; init-windmove --- tweaks for windmove mode
+
+;;; Commentary:
+
+;;; Code:
+
+;; windmove bindings clash badly with org mode, so disable them globally
+(windmove-mode -1)
+
+(provide 'init-windmove)
+;;; init-windmove.el ends here
