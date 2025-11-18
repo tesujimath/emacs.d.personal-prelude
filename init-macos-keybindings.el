4210b26ad5499b027fc5519a050238c47776458d
Simon Guest <simon.guest@tesujimath.org>
2025-11-19 11:14:12 +1300

    Standard MacOS keybinding for toggle-frame-fullscreen

---
+++ init-macos-keybindings.el
@@ -0,0 +1,9 @@
+;;; init-macos-keybindings.el --- Mac friendly keybindings -*-
+;;; Commentary:
+;;; Code:
+
+;; Use MacOS standard for toggle-frame-fullscreen
+(global-set-key (kbd "C-s-f") 'toggle-frame-fullscreen)
+
+(provide 'init-macos-keybindings)
+;;; init-macos-keybindings.el ends here
