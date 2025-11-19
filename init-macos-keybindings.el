63d9ee7caaca59bcf50ca657170555d2d337076d
Simon Guest <simon.guest@tesujimath.org>
2025-11-19 17:17:04 +1300

    Home/end line-oriented not doc oriented

--- init-macos-keybindings.el
+++ init-macos-keybindings.el
@@ -5,5 +5,9 @@
 ;; Use MacOS standard for toggle-frame-fullscreen
 (global-set-key (kbd "C-s-f") 'toggle-frame-fullscreen)
 
+;; can't get used to home/end being whole doc, so reset to line-oriented
+(global-set-key [home] 'move-beginning-of-line)
+(global-set-key [end] 'move-end-of-line)
+
 (provide 'init-macos-keybindings)
 ;;; init-macos-keybindings.el ends here
